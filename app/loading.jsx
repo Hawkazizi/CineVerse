08dca23ac630024fb10cd0d207ff9758abb9479b
Shahin <hawk.azizi1@gmail.com>
2023-08-14 23:34:55 +0330

    add Loader file

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center">
+      <img className="h-96" src="DualRing.svg" alt="Loading..." />
+    </div>
+  );
+};
+
+export default Loading;
